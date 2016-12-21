81242cb4d8422a8342f798d640e18c5be7c4e38e
m4h7 <m4h7@users.noreply.github.com>
2016-12-21 09:20:51 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
 setup(name='flatfile',
-      version='1.1',
+      version='1.2.2',
       description='Simple structured binary flatfile reader/writer',
       license='MIT',
       packages=['flatfile'],
+      install_requires=['lz4'],
       setup_requires=['pytest-runner'],
       tests_require=['pytest'],
       zip_safe=False)
